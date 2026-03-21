ada9b3bb9622c46e19c6afb4c0ba1ac671f56bea
tmpstpdwn <tmpstpdwn@proton.me>
2026-03-21 12:26:20 +0530

    Final commit from sway, moving to gnome. :(

--- .config/sway/scripts/set-bg.sh
+++ .config/sway/scripts/set-bg.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-swaybg -i $(cat $HOME/.bg) -m fill
+BG_FILE="$HOME/.bg"
+
+if [ -n "$1" ]; then
+    echo "$1" >"$BG_FILE"
+    swaybg -i "$1" -m fill &
+else
+    if [ -f "$BG_FILE" ]; then
+        swaybg -i "$(cat "$BG_FILE")" -m fill &
+    else
+        echo "No background file found!"
+        exit 1
+    fi
+fi
